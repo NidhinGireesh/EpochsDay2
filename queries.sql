3ba9b04992e5ba9abdec863380efaa0c4e34b6e5
Nidhin Gireesh <146074291+NidhinGireesh@users.noreply.github.com>
2026-07-23 19:48:51 +0530

    Refactor SQL queries for consistency and accuracy
    Updated SQL queries to use 'OrderDetails' instead of 'Order Details' and adjusted calculations for total sold and revenue.

--- queries.sql
+++ queries.sql
@@ -1,42 +1,46 @@
 SELECT
     p.ProductName,
-    SUM(od.Quantity) AS TotalSold
-FROM "Order Details" od
+    SUM(od.Quantity) AS TotalQuantitySold
+FROM OrderDetails od
 JOIN Products p
 ON od.ProductID = p.ProductID
 GROUP BY p.ProductName
-ORDER BY TotalSold DESC
+ORDER BY TotalQuantitySold DESC
 LIMIT 10;
+
+
 
 SELECT
     c.CompanyName,
-    ROUND(SUM(od.UnitPrice * od.Quantity * (1-od.Discount)),2) AS Revenue
+    ROUND(SUM(od.Quantity * od.UnitPrice),2) AS Revenue
 FROM Customers c
 JOIN Orders o
-ON c.CustomerID=o.CustomerID
-JOIN "Order Details" od
-ON o.OrderID=od.OrderID
+ON c.CustomerID = o.CustomerID
+JOIN OrderDetails od
+ON o.OrderID = od.OrderID
 GROUP BY c.CompanyName
 ORDER BY Revenue DESC
 LIMIT 10;
 
+
+
 SELECT
-    strftime('%Y-%m',o.OrderDate) AS Month,
-    ROUND(SUM(od.UnitPrice*od.Quantity*(1-od.Discount)),2) AS Sales
+    strftime('%Y-%m', o.OrderDate) AS Month,
+    ROUND(SUM(od.Quantity * od.UnitPrice),2) AS Sales
 FROM Orders o
-JOIN "Order Details" od
-ON o.OrderID=od.OrderID
+JOIN OrderDetails od
+ON o.OrderID = od.OrderID
 GROUP BY Month
 ORDER BY Month;
 
 SELECT
     c.CategoryName,
-    ROUND(SUM(od.UnitPrice*od.Quantity*(1-od.Discount)),2) AS Revenue
+    ROUND(SUM(od.Quantity * od.UnitPrice),2) AS Revenue
 FROM Categories c
 JOIN Products p
-ON c.CategoryID=p.CategoryID
-JOIN "Order Details" od
-ON p.ProductID=od.ProductID
+ON c.CategoryID = p.CategoryID
+JOIN OrderDetails od
+ON p.ProductID = od.ProductID
 GROUP BY c.CategoryName
 ORDER BY Revenue DESC;
 
@@ -45,6 +49,6 @@ SELECT
     COUNT(o.OrderID) AS NumberOfOrders
 FROM Customers c
 JOIN Orders o
-ON c.CustomerID=o.CustomerID
+ON c.CustomerID = o.CustomerID
 GROUP BY c.CompanyName
 ORDER BY NumberOfOrders DESC;
